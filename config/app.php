bd520a7e7dbc56eac300fd0263fce521761ba83e
Irina <vasilitairina@gmail.com>
2021-01-14 14:06:26 +0200

    Fixes

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'The Devs Room'),
+    'name' => env('APP_NAME', 'TheDevsRoom'),
 
     /*
     |--------------------------------------------------------------------------
